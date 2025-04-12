45d3df4c5d01b9e6bb8cd983bc029cba0ba525c8
khansatanveer <160322911+khansatanveer@users.noreply.github.com>
2025-04-12 14:14:26 +0500

    Update library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -28,7 +28,7 @@ st.markdown("""
 
 /* Set the background image for the entire page */
    [data-testid="stAppViewContainer"] > .main {
-        background-image: url('https://your-image-url.com/path-to-image.jpg');
+        background-image: url('https://images.unsplash.com/photo-1580537659466-0a9bfa916a54?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
